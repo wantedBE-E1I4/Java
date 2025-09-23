b797bd79ed414b4c3112b84272886c26435317ef
betterb2etr <b2etr@naver.com>
2025-09-23 14:23:06 +0900

    bugs: result corrected

--- src/main/java/org/week1/StudentGrade.java
+++ src/main/java/org/week1/StudentGrade.java
@@ -12,15 +12,17 @@ public class StudentGrade {
         int n = sc.nextInt();
         int[] scores = new int[n];
         for (int i = 0; i < n; i++) {
-            System.out.print((i+1) + "번째 학생의 점수 입력");
+            System.out.print((i+1) + "번째 학생의 점수 입력 ");
             scores[i] = sc.nextInt();
         }
         System.out.println("\n--- 학생 등급 결과 ---");
         int score=0;
         char grade =0;
+
         for(int i=0;i<n;i++){
+            System.out.println((i + 1) + "번째 학생의 등급: " );
             //using [switch] instead of [if]
-            switch(score/10){
+            switch(scores[i]/10){
                 case 10:
                 case 9 : {
                     System.out.println("A");
@@ -39,7 +41,7 @@ public class StudentGrade {
                     System.out.println("D");
                 }
             }
-            System.out.println((i + 1) + "번째 학생의 등급: " + grade);
+
         }
 
         System.out.println("종료");
