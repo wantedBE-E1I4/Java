775bf7b827ab88fadc8cb8c10ebc418517f804ce
JSeHoone <beebea545@gmail.com>
2025-10-05 22:37:43 +0900

    feat : menuView 추가
    - productMenu에서 1번 '메뉴판 출력'을 선택했을 때 보이 화면
    - 내부적으로 InMemoryData의 Menu를 가져와서 출력함.

--- src/main/java/org/BackOffice/services/menu/view/menuView.java
+++ src/main/java/org/BackOffice/services/menu/view/menuView.java
@@ -1,4 +1,40 @@
 package org.BackOffice.services.menu.view;
 
+import java.util.Scanner;
+import org.BackOffice.domain.InMemoryData;
+import org.BackOffice.domain.InMemoryData.Menu;
+import org.BackOffice.services.menu.ProductMenu;
+
 public class menuView {
+
+    public static void view() {
+        int menuCount = Menu.values().length;
+        ProductMenu pm = new ProductMenu();
+
+        // 메뉴판 View
+        System.out.println();
+        System.out.println("메뉴판");
+        System.out.println("메뉴ID \t 이름 \t 가격");
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= menuCount; i++) {
+            sb.append(i).append("\t").append(Menu.values()[i-1]).append("\t").append(Menu.values()[i-1].price);
+            sb.append("\n");
+        }
+        System.out.println(sb);
+
+        // 선택창 View
+        System.out.println("1. 이전 페이지");
+        Scanner sc = new Scanner(System.in);
+        int selection = sc.nextInt();
+
+        switch (selection) {
+            case 1 -> {
+                pm.run();
+            }
+        }
+
+
+    }
+
 }
