939d7f7c85edfeadd5176fabd9fa52255cbaed54
Juyeong Moon <whoor2828@gmail.com>
2025-10-05 13:47:55 +0900

    feat: Menu enum 도입

---
+++ src/main/java/org/BackOffice/services/loyalty/domain/Menu.java
@@ -0,0 +1,15 @@
+package org.BackOffice.services.loyalty.domain;
+
+public enum Menu {
+    AMERICANO("아메리카노", 2000),
+    LATTE("라떼", 3000),
+    MOCHA("모카", 3500);
+
+    public final String displayName;
+    public final int price;
+
+    Menu(String displayName, int price) {
+        this.displayName = displayName;
+        this.price = price;
+    }
+}
