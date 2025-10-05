2b1fbede2e5106e4336c86c3a58034eaa23aa1b9
JSeHoone <beebea545@gmail.com>
2025-10-05 22:34:57 +0900

    feat : ProductMenu에 진입했을 때 호출 되는 메서드 정의
    - run() 메서드를 수행하면 선택할 수 있도록 Scanner정의
    - 1. 메뉴판을 확인하는 '메뉴 확인'
    - 2. 메뉴에 대한 관리를 하는 '메뉴 관리'

--- src/main/java/org/BackOffice/services/menu/ProductMenu.java
+++ src/main/java/org/BackOffice/services/menu/ProductMenu.java
@@ -1,4 +1,30 @@
 package org.BackOffice.services.menu;
 
+import java.util.Scanner;
+import org.BackOffice.services.menu.view.menuView;
+
 public class ProductMenu {
+
+    /**
+     * ProductMenu에 진입하기 위한 실행 메서드
+    * */
+    public void run() {
+        System.out.println();
+        System.out.println("메뉴 관리자");
+        System.out.println("1.메뉴판 확인 \t 2.메뉴 관리");
+        Scanner sc = new Scanner(System.in);
+        int selection = sc.nextInt();
+
+
+        switch (selection) {
+            case 1 -> {
+                System.out.println("메뉴판 출력");
+                menuView.view();
+            }
+            case 2 -> {
+                System.out.println("메뉴 관리 탭 출력");
+            }
+        }
+
+    }
 }
