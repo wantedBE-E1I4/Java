5e7480ede245024de9d32f5a1dd13ca2e16d0025
Juyeong Moon <whoor2828@gmail.com>
2025-10-06 00:44:10 +0900

    feat: Order에 id/guestId/아이템 컬렉션 및 합계 계산 로직 추가

--- src/main/java/org/BackOffice/services/loyalty/domain/Order.java
+++ src/main/java/org/BackOffice/services/loyalty/domain/Order.java
@@ -1,9 +1,35 @@
 package org.BackOffice.services.loyalty.domain;
 
-public class Order {
-    private final int guest_id;
+import java.util.ArrayList;
+import java.util.List;
 
-    public Order(int id) {
-        this.guest_id = id;
+public class Order {
+    private static int seq = 0;
+    private final int id = ++seq;
+    private final int guestId;
+    private static int totalPay = 0;
+
+    List<OrderItem> items = new ArrayList<>();
+
+    public Order(int guestId) {
+        this.guestId = guestId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void addItem(int menuId, int quantity) {
+        OrderItem orderItem = new OrderItem(menuId, quantity);
+        items.add(orderItem);
+        getTotal(orderItem.quantity, orderItem.price);
+    }
+
+    private void getTotal(int quantity, int price) {
+        totalPay += (quantity * price);
+    }
+
+    public int getTotalPay() {
+        return totalPay;
     }
 }
