a71c9b013633e9682bee91d7a7c60db0ba2ca8ab
JSeHoone <beebea545@gmail.com>
2025-10-06 16:09:58 +0900

    feat : menuManagerView 추가
    - 메뉴 관리 뷰
    - 1. 메뉴판 상태 관리뷰(메뉴의 가격 설정 및 상태(판매중, 등등))
    - 2. 메뉴 등록 및 삭제뷰(신메뉴를 등록하거나 기존 메뉴를 삭제하는 뷰)

---
+++ src/main/java/org/BackOffice/services/menu/view/menuManagerView.java
@@ -0,0 +1,34 @@
+package org.BackOffice.services.menu.view;
+
+import java.util.Scanner;
+import org.BackOffice.services.menu.ProductMenu;
+
+public class menuManagerView {
+    // Test를 위한 main 메서드
+    public static void main(String[] args) {
+        view();
+    }
+    public static void view() {
+        ProductMenu pm = new ProductMenu();
+
+        // 출력 화면
+        System.out.println();
+        System.out.println("메뉴 관리");
+        System.out.println("1. 메뉴판 상태관리 \t 2. 메뉴 등록 및 삭제 \t 3. 이전 페이지");
+        Scanner sc = new Scanner(System.in);
+        int selection = sc.nextInt();
+
+
+        switch (selection) {
+            case 1 -> {
+                System.out.println("메뉴판 상태관리");
+            }
+            case 2 -> {
+                System.out.println("메뉴 등록");
+            }
+            case 3 -> {
+                pm.run();
+            }
+        }
+    }
+}
