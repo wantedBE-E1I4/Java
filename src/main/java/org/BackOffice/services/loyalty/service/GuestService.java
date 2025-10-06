696f6131738ca8ead2e45f561911b71f20e2f160
Juyeong Moon <whoor2828@gmail.com>
2025-10-06 22:22:42 +0900

    feat: findGuest 메서드 추가

--- src/main/java/org/BackOffice/services/loyalty/service/GuestService.java
+++ src/main/java/org/BackOffice/services/loyalty/service/GuestService.java
@@ -1,13 +1,20 @@
 package org.BackOffice.services.loyalty.service;
 
+import org.BackOffice.services.loyalty.MembershipCouponMenu;
 import org.BackOffice.services.loyalty.domain.Guest;
+import org.BackOffice.services.loyalty.repository.GuestRepository;
 
 /**
  * ID를 발급하고 게스트를 생성
  */
 public class GuestService {
-    public int createGuest() {
+    public int createGuest(GuestRepository guestRepository) {
         Guest guest = new Guest();
+        guestRepository.save(guest);
         return guest.getId();
+    }
+
+    public Guest findGuest(GuestRepository guestRepository, int guestId) {
+        return guestRepository.findById(guestId);
     }
 }
