b2f1faa5222415d5a45965d66cffd2e3b88aa893
JSeHoone <beebea545@gmail.com>
2025-10-06 18:50:49 +0900

    refactor : ProductMenu로 진입시 ProductLoader로 동기화 하기

--- src/main/java/org/BackOffice/services/menu/ProductMenu.java
+++ src/main/java/org/BackOffice/services/menu/ProductMenu.java
@@ -1,6 +1,7 @@
 package org.BackOffice.services.menu;
 
 import java.util.Scanner;
+import org.BackOffice.services.menu.loader.ProductMenuLoader;
 import org.BackOffice.services.menu.view.MenuBoardView;
 import org.BackOffice.services.menu.view.MenuManagerView;
 
@@ -10,6 +11,10 @@ public class ProductMenu {
      * ProductMenu에 진입하기 위한 실행 메서드
     * */
     public void run() {
+        // 들어오면 InMemoryData에 있는 정보를 MenuTable에 동기화하기
+        ProductMenuLoader.syncMenuTable();
+
+        // 출력하기
         System.out.println();
         System.out.println("메뉴 관리자");
         System.out.println("1.메뉴판 확인 \t 2.메뉴 관리");
