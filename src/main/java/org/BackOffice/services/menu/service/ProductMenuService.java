36e55f025f0fd43672eb9ee1ccf50abb747cef33
JSeHoone <beebea545@gmail.com>
2025-10-08 22:55:45 +0900

    feat : 메뉴 얻어오는 로직 수정 및 업데이트 로직 추가
    - getMenuList() : 기존 전체를 가져오는 로직에서 삭제 상태는 안가져오도록 수정
    - updateMenuPrice() : 메뉴의 가격을 변경하는 로직
    - updateMenuStatus() : 메뉴의 상태값을 변경하는 로직

--- src/main/java/org/BackOffice/services/menu/service/ProductMenuService.java
+++ src/main/java/org/BackOffice/services/menu/service/ProductMenuService.java
@@ -2,11 +2,35 @@ package org.BackOffice.services.menu.service;
 
 import static org.BackOffice.services.menu.tables.MenuTable.menuTable;
 
+import java.util.HashMap;
 import java.util.Map;
 import org.BackOffice.services.menu.domain.MenuEntity;
+import org.BackOffice.services.menu.domain.MenuStatus;
 
 public class ProductMenuService {
     public Map<Integer, MenuEntity> getMenuList() {
-        return menuTable;
+        // 삭제 상태의 메뉴는 보여주지 않아야 함 !
+        Map<Integer, MenuEntity> response = new HashMap<>();
+        for (Integer key : menuTable.keySet()) {
+            MenuEntity data = menuTable.get(key);
+            if (data.getMenuStatus().statusValue != 4) {
+                response.put(key, data);
+            }
+        }
+        return response;
+    }
+
+    public MenuEntity updateMenuPrice(int id, int price) {
+        MenuEntity findEntity = menuTable.get(id);
+        findEntity.setMenuPrice(price);
+
+        return findEntity;
+    }
+
+    public MenuEntity updateMenuStatus(int id, MenuStatus status) {
+        MenuEntity findEntity = menuTable.get(id);
+        findEntity.setMenuStatus(status);
+
+        return findEntity;
     }
 }
