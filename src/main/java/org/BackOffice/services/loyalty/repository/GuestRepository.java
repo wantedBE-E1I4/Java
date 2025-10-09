2ffe15e898be8b738262e2d8b85208ca73da48aa
Juyeong Moon <whoor2828@gmail.com>
2025-10-09 20:07:43 +0900

    refactor: findById를 key 기반 조회로 변경

--- src/main/java/org/BackOffice/services/loyalty/repository/GuestRepository.java
+++ src/main/java/org/BackOffice/services/loyalty/repository/GuestRepository.java
@@ -2,26 +2,18 @@ package org.BackOffice.services.loyalty.repository;
 
 import org.BackOffice.services.loyalty.domain.Guest;
 
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 public class GuestRepository {
-    private Map<Integer, Guest> guestMap = new HashMap<>();
-    int seq = 0;
+    private final Map<Integer, Guest> guestMap = new HashMap<>();
+    static int seq = 100;
 
-    public Guest findById(int id) {
-        Collection<Guest> values = guestMap.values();
-        for (Guest value : values) {
-            if (id == value.getId()) {
-                return value;
-            }
-        }
-        return null;
+    public Guest findById(int guestId) {
+        return guestMap.get(guestId);
     }
-    //Repository가 id 생성 책임을 갖게 한다.
+
     public void save(Guest guest) {
-        //id 발급
         guest.setId(++seq);
         guestMap.put(guest.getId(), guest);
     }
