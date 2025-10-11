1f556360f8bedc1d776c9951934fddb4fa40ced0
JSeHoone <beebea545@gmail.com>
2025-10-11 19:14:52 +0900

    refactor : Repository 생성으로 인한 Service로직 수정
    - 기존 table내 menuTable 의존성을 제거
    - Repository를 통해 데이터 저장 변수에 대한 의존성 제거
    - MenuRepository에서 제공하는 메서드로 데이터 핸들링 하도록 로직 수정

--- src/main/java/org/BackOffice/services/menu/service/ProductMenuService.java
+++ src/main/java/org/BackOffice/services/menu/service/ProductMenuService.java
@@ -1,61 +1,96 @@
 package org.BackOffice.services.menu.service;
 
-import static org.BackOffice.services.menu.tables.MenuTable.currentId;
-import static org.BackOffice.services.menu.tables.MenuTable.menuTable;
-
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import org.BackOffice.services.menu.domain.MenuEntity;
 import org.BackOffice.services.menu.domain.MenuStatus;
+import org.BackOffice.services.menu.repository.MemoryMenuRepository;
+import org.BackOffice.services.menu.repository.MenuRepository;
 
+/*
+* [역할]
+* - 메뉴들을 관리하기 위한 비즈니스 서비스들을 제공합니다.
+*
+* [책임]
+* - 메뉴 관련 비즈니스 로직들을 제공한다.
+*
+* [설계의도]
+* - Repository와 연결되어 데이터를 다룹니다.
+*
+* */
 public class ProductMenuService {
-    public Map<Integer, MenuEntity> getMenuList() {
+    private final MenuRepository repo = new MemoryMenuRepository();
+
+    /**
+     * 메뉴판에 제공되고 있는 메뉴들을 반환합니다.
+     * @return 삭제 상태를 제외한 메뉴 리스트
+    * */
+    public List<MenuEntity> getMenuList() {
         // 삭제 상태의 메뉴는 보여주지 않아야 함 !
-        Map<Integer, MenuEntity> response = new HashMap<>();
-        for (Integer key : menuTable.keySet()) {
-            MenuEntity data = menuTable.get(key);
-            if (data.getMenuStatus().statusValue != 4) {
-                response.put(key, data);
-            }
-        }
-        return response;
+        List<MenuEntity> findMenuList = repo.findAll();
+        findMenuList.removeIf(menuEntity -> menuEntity.getMenuStatus().statusCode == 4);
+        return findMenuList;
     }
 
-    public MenuEntity updateMenuPrice(int id, int price) {
-        MenuEntity findEntity = menuTable.get(id);
-        if (findEntity == null) {
-            System.out.println("존재하지 않는 메뉴입니다.");
-            return null;
-        }
+    /**
+     * 메뉴의 가격을 변경합니다.
+     * @param id 메뉴의 id값
+     * @param price 변경하고자 하는 가격
+     * @return 변경된 값의 MenuEntity
+    * */
+    public MenuEntity updateMenuPrice(long id, int price) {
+        MenuEntity findEntity = repo.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("존재하지 않는 메뉴입니다.")
+        );
+
         findEntity.setMenuPrice(price);
 
-        return findEntity;
+        return repo.save(findEntity);
     }
 
-    public MenuEntity updateMenuStatus(int id, MenuStatus status) {
-        MenuEntity findEntity = menuTable.get(id);
-        if (findEntity == null) {
-            System.out.println("존재하지 않는 메뉴입니다.");
-            return null;
-        }
+    /**
+     * 메뉴의 상태를 변경합니다.
+     * @param id 메뉴의 id값
+     * @param status MenuStatus의 값
+     * @return 변경된 값의 MenuEntity
+    * */
+    public MenuEntity updateMenuStatus(long id, MenuStatus status) {
+        MenuEntity findEntity = repo.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("존재하지 않는 메뉴입니다.")
+        );
+
         findEntity.setMenuStatus(status);
 
-        return findEntity;
+        return repo.save(findEntity);
     }
 
-    public void registerNewMenu(String name, int price) {
-        MenuEntity newMenu = MenuEntity.createMenuEntity(name,price,MenuStatus.신규);
-        menuTable.put(++currentId,newMenu);
-    }
-
-    public MenuEntity removeMenu(int id) {
-        MenuEntity findMenu = menuTable.get(id);
-        if (findMenu == null) {
-            System.out.println("존재하지 않는 메뉴입니다.");
-            return null;
+    /**
+     * 새로운 메뉴를 등록합니다.
+     * @param name 새로 등록할 메뉴의 이름
+     * @param price 등록할 메뉴의 가격
+     * @return 새로 등록된 MenuEntity
+    * */
+    public MenuEntity registerNewMenu(String name, int price) {
+        if (repo.findByName(name).isPresent()) {
+            throw new IllegalArgumentException("이미 존재하는 메뉴입니다: " + name);
         }
 
-        menuTable.remove(id);
-        return findMenu;
+        MenuEntity newMenu = MenuEntity.createMenuEntity(name, price, MenuStatus.NEW);
+        return repo.save(newMenu);
+    }
+
+    /**
+     * 메뉴를 삭제합니다.
+     * @param id 삭제할 메뉴의 id값
+     * @return 삭제된 menu의
+    * */
+    public MenuEntity removeMenu(long id) {
+        MenuEntity deletedMenu = repo.delete(id).orElseThrow(
+                () -> new IllegalArgumentException("존재하지 않는 메뉴입니다.")
+        );
+
+        return deletedMenu;
     }
 }
