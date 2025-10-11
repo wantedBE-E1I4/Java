9c3a75a1d37c7a98b70c1b442f1c3d2405e4d254
JSeHoone <beebea545@gmail.com>
2025-10-11 19:07:17 +0900

    feat : repository 패키지 추가
    - 기존 table 패키지의 역할을 대체
    - 확장 가능하도록 interface구현
    - Memory에서 관리하는 MemoryMenuRepository 구현

---
+++ src/main/java/org/BackOffice/services/menu/repository/MemoryMenuRepository.java
@@ -0,0 +1,43 @@
+package org.BackOffice.services.menu.repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import org.BackOffice.services.menu.domain.MenuEntity;
+
+public class MemoryMenuRepository implements MenuRepository{
+
+    private static Map<Long, MenuEntity> menuStore = new HashMap<>();
+    private static Long autoIncrement = 0L;
+
+    @Override
+    public MenuEntity save(MenuEntity menu) {
+        menuStore.put(++autoIncrement,menu);
+        return menu;
+    }
+
+    @Override
+    public Optional<MenuEntity> findById(Long id) {
+        return Optional.of(menuStore.get(id));
+    }
+
+    @Override
+    public Optional<MenuEntity> findByName(String name) {
+        return menuStore.values().stream()
+                .filter(menu -> menu.getMenuName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<MenuEntity> findAll() {
+        return new ArrayList<>(menuStore.values());
+    }
+
+    @Override
+    public Optional<MenuEntity> delete(Long id) {
+        MenuEntity deletedEntity = menuStore.remove(id);
+        return Optional.of(deletedEntity);
+    }
+}
