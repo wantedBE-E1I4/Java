746abbbda1c934a7832a6289129338c7d41ab550
Juyeong Moon <whoor2828@gmail.com>
2025-10-11 15:15:07 +0900

    feat: LoyaltyAccount 생성 시 결제금액의 10%를 적립하도록 로직 추가

--- src/main/java/org/BackOffice/services/loyalty/domain/LoyaltyAccount.java
+++ src/main/java/org/BackOffice/services/loyalty/domain/LoyaltyAccount.java
@@ -3,4 +3,19 @@ package org.BackOffice.services.loyalty.domain;
 public class LoyaltyAccount {
     private int guestId;
     private int balance;
+
+    public LoyaltyAccount(int guestId, int pay) {
+        this.guestId = guestId;
+        balance += cal(pay);
+    }
+
+    public int getGuestId() {
+        return guestId;
+    }
+
+    private int cal (int pay) {
+        return (int) (pay * 0.10);
+    }
 }
+
+
