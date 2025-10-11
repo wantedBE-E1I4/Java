4ccabff41bd9ac7643ac21f3901e08fb7576db3a
JSeHoone <beebea545@gmail.com>
2025-10-11 19:11:56 +0900

    refactor : 기존 enum 이름을 한국어에서 영어로 변경
    - enum 타입의 이름을 영어로 변경
    - 내부 값에 statusValue로 한국어 설정
    - 해당 값을 관리하기 편하도록 statusCode값 추가

--- src/main/java/org/BackOffice/services/menu/domain/MenuStatus.java
+++ src/main/java/org/BackOffice/services/menu/domain/MenuStatus.java
@@ -1,13 +1,17 @@
 package org.BackOffice.services.menu.domain;
 
 public enum MenuStatus {
-    판매중(1),
-    품절(2),
-    신규(3),
-    삭제(4);
+    SELLING("판매중",1),
+    SOLDOUT("품절",2),
+    NEW("신규",3),
+    DELETE("삭제",4);
 
-    public final int statusValue;
-    MenuStatus(int statusValue) {
+
+    public final String statusValue;
+    public final int statusCode;
+
+    MenuStatus(String statusValue, int statusCode) {
         this.statusValue = statusValue;
+        this.statusCode = statusCode;
     }
 }
