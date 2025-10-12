05dcfdb82d26326db4d6d015527e8d5dfce8cdd1
Juyeong Moon <whoor2828@gmail.com>
2025-10-12 16:59:32 +0900

    feat: getOrCreateForGuest(guestId) 추가로 계정 존재 보장 - guestId로 조회 후 없으면 balance=0인 LoyaltyAccount 생성·저장 뒤 반환(절대 null 아님) - 불필요한 Guest import 제거, 레포지토리 책임 명확화

--- src/main/java/org/BackOffice/services/loyalty/repository/LoyaltyRepository.java
+++ src/main/java/org/BackOffice/services/loyalty/repository/LoyaltyRepository.java
@@ -1,6 +1,5 @@
 package org.BackOffice.services.loyalty.repository;
 
-import org.BackOffice.services.loyalty.domain.Guest;
 import org.BackOffice.services.loyalty.domain.LoyaltyAccount;
 
 import java.util.HashMap;
@@ -15,5 +14,15 @@ public class LoyaltyRepository {
 
     public void save(LoyaltyAccount account) {
         loyaltyMap.put(account.getGuestId(), account);
+    }
+
+    public LoyaltyAccount getOrCreateForGuest(int guestId) {
+        LoyaltyAccount account = findById(guestId);
+        if (account != null) {
+            return account;
+        }
+        LoyaltyAccount newAccount = new LoyaltyAccount(guestId);
+        save(newAccount);
+        return newAccount;
     }
 }
